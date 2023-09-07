2238e9fb225a070965d2b5b6812e79bcf877eae7
BKSalman <salman.f.abuhaimed@gmail.com>
2023-09-08 01:14:22 +0300

    volume

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -34,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_GRV,
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-        TD(TD_LSFT_CAPS), KC_A ,KC_S ,KC_D, KC_F, KC_G,                     KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
+        TD(TD_LSFT_CAPS), KC_A ,KC_S ,KC_D, KC_F, KC_G,                          KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
                          KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                          RAISE,KC_SPC,                           KC_BSPC, LOWER,
                                          KC_LALT,KC_LGUI,                        KC_ENT,  KC_DEL,
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_5x6(
           KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11,
-          _______,_______,_______,_______,_______,_______,                        _______,_______,KC_NUM ,KC_LBRC,KC_RBRC,KC_MUTE,
-          _______,KC_LCBR,KC_RCBR,KC_LPRN,KC_RPRN,_______,                        KC_LEFT,KC_DOWN  ,KC_UP,KC_RGHT,_______,KC_VOLU,
+          _______,_______,_______,_______,_______,_______,                        _______,_______,KC_NUM ,KC_LBRC,KC_RBRC,KC_VOLU,
+          _______,KC_LCBR,KC_RCBR,KC_LPRN,KC_RPRN,_______,                        KC_LEFT,KC_DOWN  ,KC_UP,KC_RGHT,_______,KC_MUTE,
           _______,_______,_______,_______,_______,_______,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLD,
                                   _______,MUTE_OBS,                                                 _______ ,_______,
                                                   _______,_______,            _______,_______,
